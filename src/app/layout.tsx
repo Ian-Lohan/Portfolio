da0f438778c531d158c56c4e59d15b8c0ec7487f
Ian Lohan <ianlohan.acc@gmail.com>
2024-02-12 16:35:53 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ian Lohan",
+  description: "Sou um desenvolvedor web e mobile.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

2a8544c55a02ecf23010ed5fdd20bef517f3a0e8
Ian Lohan <ianlohan.acc@gmail.com>
2024-02-14 00:05:03 -0300

    Scrollable Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,49 +25,44 @@ export default function Home() {
                 </div>
             </div>
             <div className="sections">
-            <h1 class="section-header">About Me</h1>
+                <h1 class="section-header">About Me</h1>
                 <div className="about-me">
-                    <img src="./photo.jpg" alt="My Photo"/>
+                    <img src="./photo.jpg" alt="My Photo" />
                     <p>
-                        Hi , I'm Ian Lohan, a Front-End Developer from Pernambuco,
-                        Brazil.
+                        Hi , I'm Ian Lohan, a Front-End Developer from
+                        Pernambuco, Brazil.
+                        <br></br>
+                        <br></br>I have a passion for web development and love
+                        to create web applications with a focus on user
+                        experience.
                         <br></br>
                         <br></br>
-                        I have a passion for web development and love to
-                        create web applications with a focus on user experience. 
+                        My programming journey started in 2020 when I first got
+                        into college, I was introduced to a wide variety of
+                        programming languages by my professors and friends, and
+                        since then I've been constantly learning new
+                        technologies and improving my skills as a programmer.
+                        <br></br>
+                        <br></br>I am currently studying Computer Science at
+                        UniFavip Wyden and taking multiple courses online, such
+                        as CS50x, FreeCodeCamp and The Odin Project.
                         <br></br>
                         <br></br>
-                        My programming journey started in 2020 when I first got into college,
-                        I was introduced to a wide variety of programming languages by my 
-                        professors and friends, and since then I've been constantly learning
-                        new technologies and improving my skills as a programmer. 
-                        <br></br>
-                        <br></br>
-                        I am currently
-                        studying Computer Science at UniFavip Wyden and taking multiple courses
-                        online, such as CS50x, FreeCodeCamp and The Odin Project.
-                        <br></br>
-                        <br></br>
-                        My goal is to become a Full-Stack Developer and create all sorts of web applications
-                        to help people in their daily lives.
+                        My goal is to become a Full-Stack Developer and create
+                        all sorts of web applications to help people in their
+                        daily lives.
                     </p>
                 </div>
                 <h1>Skills</h1>
                 <div className="skills">
-                    <img src="./html.png" alt="HTML"/>
-                    <img src="./css.png" alt="CSS"/>
-                    <img src="./javascript.png" alt="JavaScript"/>
-                    <img src="./typescript.png" alt="TypeScript"/>
-                    <img src="./react.png" alt="React"/>
-                    <img src="./sass.png" alt="Sass"/>
+                    <img src="./html.png" alt="HTML" />
+                    <img src="./css.png" alt="CSS" />
+                    <img src="./javascript.png" alt="JavaScript" />
+                    <img src="./typescript.png" alt="TypeScript" />
+                    <img src="./react.png" alt="React" />
+                    <img src="./node js.png" alt="Node JS" />
+                    <img src="./sass.png" alt="Sass" />
                 </div>
-                <Image
-                    src="/vercel.svg"
-                    alt="Vercel Logo"
-                    width={100}
-                    height={24}
-                    priority
-                />
             </div>
         </main>
     );
